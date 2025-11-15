24bd7ce8a607bf989f50d6c4f50805d0bceda91a
IOSOZ <oleg.zuev.2012@gmail.com>
2025-11-16 02:49:12 +0300

    Work with CoreData

---
+++ ArchitecturePatternsAppV2/VIPER/Core/Entites/Pattern+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Pattern+CoreDataClass.swift
+//  ArchitecturePatternsAppV2
+//
+//  Created by Олег Зуев on 13.11.2025.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Pattern)
+public class Pattern: NSManagedObject {
+
+}
